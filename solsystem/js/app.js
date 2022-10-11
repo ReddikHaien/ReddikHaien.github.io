0d1c90509cd8371e73dddee81b40d83e213b349b
ReddikHaien <584969@stud.hvl.no>
2022-10-11 13:40:59 +0200

    added solar system

---
+++ solsystem/js/app.js
@@ -0,0 +1,51 @@
+"use strict"
+
+import {Color, PerspectiveCamera, Scene, WebGLRenderer} from "./build/three.module.js";
+import SolarSystem from "./solarSystem.js";
+import {VRButton} from "./build/VRButton.js";
+
+export default class App{
+
+    constructor() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.aspect = this.width / this.height;
+
+        this.fov = 75;
+        this.near = 0.1;
+        this.far = 1000.0;
+
+        this.camera = new PerspectiveCamera(this.fov,this.aspect,this.near,this.far);
+
+        this.camera.position.z = 50;
+
+        this.scene = new Scene();
+
+        const canvas = document.createElement("canvas");
+        const gl = canvas.getContext("webgl2");
+        this.renderer = new WebGLRenderer({
+            canvas,
+            context: gl,
+        });
+
+        this.renderer.setClearColor(new Color(0,0,0));
+        this.renderer.setSize(this.width,this.height);
+
+        this.renderer.xr.enabled = true;
+        document.body.append(VRButton.createButton(this.renderer));
+        document.body.appendChild(this.renderer.domElement);
+
+        this.solarSystem = new SolarSystem(this.scene);
+
+        this.render_closure = this.render.bind(this);
+        this.renderer.setAnimationLoop(this.render_closure);
+    }
+
+    render(){
+        this.renderer.render(this.scene,this.camera);
+
+        this.solarSystem.animate();
+    }
+}
+
+new App();
---
+++ solsystem/js/solarSystem.js
@@ -0,0 +1,69 @@
+"use strict"
+import {
+    AmbientLight,
+    Color,
+    Mesh,
+    MeshBasicMaterial,
+    MeshPhongMaterial,
+    Object3D, PointLight,
+    SphereGeometry,
+    TextureLoader
+} from "./build/three.module.js";
+
+export default class SolarSystem{
+    /**
+     *
+     * @param {Scene} scene
+     */
+    constructor(scene){
+        let radius = 5;
+        let widthSegments = 64;
+        let heightSegments = 64;
+
+        this.root = new Object3D();
+        this.root.position.z = -50;
+        scene.add(this.root);
+
+        let sunGeometry = new SphereGeometry(radius,widthSegments,heightSegments);
+
+        let sunTextureUrl = 'assets/texture_sun.jpg';
+        let sunTexture = new TextureLoader().load(sunTextureUrl);
+
+        let sunMaterial = new MeshBasicMaterial({
+            map: sunTexture
+        });
+
+
+        this.sun = new Mesh(sunGeometry,sunMaterial);
+        this.root.add(this.sun);
+
+        this.sunLight = new PointLight(new Color(1.0,1.0,0.9),3);
+        this.root.add(this.sunLight);
+
+        this.ambientLight = new AmbientLight(new Color(1.0,1.0,1.0),0.05);
+        this.root.add(this.ambientLight);
+        this.eartOrbit = new Object3D();
+        this.root.add(this.eartOrbit);
+
+        let earthTextureUrl = "assets/texture_earth.jpg";
+        let earthTexture = new TextureLoader().load(earthTextureUrl);
+        let earthMaterial = new MeshPhongMaterial({
+            map: earthTexture,
+            shininess: 1.0
+        });
+
+        radius = 2.5;
+        let earthGeometry = new SphereGeometry(radius,widthSegments,heightSegments);
+        this.earth = new Mesh(earthGeometry,earthMaterial);
+        this.eartOrbit.add(this.earth);
+
+        this.earth.position.x = 15;
+    }
+
+
+    animate(){
+        this.sun.rotation.y += 0.001;
+        this.eartOrbit.rotation.y += 0.005;
+        this.earth.rotation.y += 0.003;
+    }
+}
